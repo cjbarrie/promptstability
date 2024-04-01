ef4121e7a0c00d2420350e5a48f2f3d098da2296
Elli Palaiologou <elli.palaiologou@gmail.com>
2024-04-01 19:08:22 +0100

    validate script

--- notebooks/similarity_validation/validate.py
+++ notebooks/similarity_validation/validate.py
@@ -1,21 +1,24 @@
 import pandas as pd
 
 data = pd.read_csv('data.csv')
+test = data.iloc[:2].copy()
 
 # Iterate through each sentence
-for index, row in data.iterrows():
+for index, row in test.iterrows():
+    original_prompt = row['original_prompt']
     generated_prompt = row['prompt_text']
 
     # Display the sentence to the coder
-    print(f"Prompt: {generated_prompt}")
+    print(f"Original Prompt: {original_prompt}")
+    print(f"Generated Prompt: {generated_prompt}")
 
     # Prompt the coder to input their rating
-    similarity = input("Enter your similarity (from 0 to 10): ")
+    similarity = input("Enter your similarity score between original and generated prompts (min 0, max 10): ")
 
     # Store the rating along with the coder's name in a new column
     coder_name = input("Enter your name (all lowercase): ")  # Replace with the coder's name
-    data.at[index, coder_name] = similarity
+    test.loc[index, coder_name] = float(similarity)
 
-print(data)
+print(test)
 # Save the updated DataFrame back to CSV
 #data.to_csv('data_validated.csv', index=False)
